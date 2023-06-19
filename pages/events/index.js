29466f15c16a00220c0005bcffa96331264b3362
julielaursen <julielaursen1@gmail.com>
2023-06-19 15:37:31 -0500

    add search bar

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import { getAllEvents } from '../../dummy-data'
 import EventList from '../../components/events/event-list'
+import EventsSearch from '../../components/events/events-search'
 
 function AllEventsPage() {
   const events = getAllEvents()
   return (
     <div>
+      <EventsSearch></EventsSearch>
       <EventList items={events} />
     </div>
   )
